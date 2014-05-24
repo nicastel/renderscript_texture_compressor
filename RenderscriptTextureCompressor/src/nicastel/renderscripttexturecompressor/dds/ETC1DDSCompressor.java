4bfe7122bd5cd6f00c23a82974add5ce91e52d7c
nicastel <nic.castel@gmail.com>
2014-05-24 03:02:06 +0200

    bug correction DDS writer

--- RenderscriptTextureCompressor/src/nicastel/renderscripttexturecompressor/dds/ETC1DDSCompressor.java
+++ RenderscriptTextureCompressor/src/nicastel/renderscripttexturecompressor/dds/ETC1DDSCompressor.java
@@ -1,9 +1,13 @@
 package nicastel.renderscripttexturecompressor.dds;
 
 import gov.nasa.worldwind.util.ImageUtil;
+import gov.nasa.worldwind.util.Logging;
+import gov.nasa.worldwind.util.WWIO;
+import gov.nasa.worldwind.util.WWMath;
 import gov.nasa.worldwind.util.dds.DDSCompressor;
 import gov.nasa.worldwind.util.dds.DDSConstants;
 import gov.nasa.worldwind.util.dds.DDSHeader;
+import gov.nasa.worldwind.util.dds.DDSPixelFormat;
 import gov.nasa.worldwind.util.dds.DXT1Compressor;
 import gov.nasa.worldwind.util.dds.DXT3Compressor;
 import gov.nasa.worldwind.util.dds.DXTCompressionAttributes;
@@ -13,8 +17,11 @@ import java.nio.ByteBuffer;
 
 import nicastel.renderscripttexturecompressor.etc1.rs.RsETC1;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory.Options;
 
-public class ETC1DDSCompressor extends DDSCompressor {
+public class ETC1DDSCompressor {
     protected DXTCompressor getDXTCompressor(Bitmap image, DXTCompressionAttributes attributes)
     {
         // If the caller specified a DXT format in the attributes, then we return a compressor matching that format.
@@ -56,7 +63,7 @@ public class ETC1DDSCompressor extends DDSCompressor {
 		// data is accessed directly. In this case, such code would be responsible for recognizing the color model
 		// (premultiplied) and behaving accordingly.
 
-		Bitmap.Config mipmapImageType = Bitmap.Config.RGB_565;
+		Bitmap.Config mipmapImageType = Bitmap.Config.ARGB_8888;
 		int maxLevel = ImageUtil.getMaxMipmapLevel(image.getWidth(), image.getHeight());
 
 		if(attributes.getDXTFormat() == ETCConstants.D3DFMT_ETC1) {
@@ -69,7 +76,217 @@ public class ETC1DDSCompressor extends DDSCompressor {
 		
 	}
 	
-	@Override
+	/**
+	 * Convenience method to convert the specified <code>imageBuffer</code> to DDS according to the default attributes.
+	 * The bytes in <code>imageBuffer</code> must be readable by {@link BitmapFactory#decodeStream
+	 * BitmapFactory.decodeStream}. Once the image data is read, this is equivalent to calling {@link #compressImage(Bitmap)} with the Bitmap created by
+	 * BitmapFactory. This returns null if the bytes
+	 * in<code>imageBuffer</code> are not in a format understood by BitmapFactory.
+	 * 
+	 * @param imageBuffer
+	 *            image file data to convert to the DDS file format.
+	 * @return little endian ordered ByteBuffer containing the DDS file bytes, or null if the <code>imageBuffer</code> is not in a format understood by
+	 *         BitmapFactory.
+	 * @throws IllegalArgumentException
+	 *             if <code>imageBuffer</code> is null.
+	 */
+	public static java.nio.ByteBuffer compressImageBuffer(java.nio.ByteBuffer imageBuffer) {
+		if (imageBuffer == null) {
+			String message = Logging.getMessage("nullValue.Image");
+			Logging.error(message);
+			throw new IllegalArgumentException(message);
+		}
+
+		return compressImageBuffer(imageBuffer, DDSCompressor.getDefaultCompressionAttributes());
+	}
+	
+	/**
+	 * Convenience method to convert the specified <code>imageBuffer</code> to DDS according to the specified
+	 * compression <code>attributes</code>. The bytes in <code>imageBuffer</code> must be readable by {@link BitmapFactory#decodeStream
+	 * BitmapFactory.decodeStream}. Once the image data is read, this is equivalent to
+	 * calling {@link #compressImage(Bitmap, DXTCompressionAttributes)} with the Bitmap created with the specified <code>attributes</code>. This returns null if
+	 * the bytes in <code>imageBuffer</code> are not in a format
+	 * understood by BitmapFactory.
+	 * 
+	 * @param imageBuffer
+	 *            image file data to convert to the DDS file format.
+	 * @param attributes
+	 *            attributes that control the compression.
+	 * @return little endian ordered ByteBuffer containing the DDS file bytes, or null if the <code>imageBuffer</code> is not in a format understood by
+	 *         BitmapFactory.
+	 * @throws IllegalArgumentException
+	 *             if either <code>imageBuffer</code> or <code>attributes</code> are null.
+	 */
+	public static java.nio.ByteBuffer compressImageBuffer(java.nio.ByteBuffer imageBuffer, DXTCompressionAttributes attributes) {
+		if (imageBuffer == null) {
+			String message = Logging.getMessage("nullValue.Image");
+			Logging.error(message);
+			throw new IllegalArgumentException(message);
+		}
+
+		if (attributes == null) {
+			String message = Logging.getMessage("nullValue.AttributesIsNull");
+			Logging.error(message);
+			throw new IllegalArgumentException(message);
+		}
+
+		java.io.InputStream inputStream = WWIO.getInputStreamFromByteBuffer(imageBuffer);
+		return compressImageStream(inputStream, attributes);
+	}
+	
+	/**
+	 * Convenience method to convert the specified image <code>stream</code> to DDS according to the specified
+	 * compression <code>attributes</code>. The <code>stream</code> must be readable by {@link BitmapFactory#decodeStream BitmapFactory.decodeStream}. Once the
+	 * <code>stream</code> is read, this is equivalent
+	 * to calling {@link #compressImage(Bitmap, DXTCompressionAttributes)} with the Bitmap created with the specified <code>attributes</code>. This returns null
+	 * if the <code>stream</code> is not in a format understood by
+	 * BitmapFactory.
+	 * 
+	 * @param inputStream
+	 *            image stream to convert to the DDS file format.
+	 * @param attributes
+	 *            attributes that control the compression.
+	 * @return little endian ordered ByteBuffer containing the DDS file bytes, or null if the <code>stream</code> is not
+	 *         in a format understood by BitmapFactory.
+	 * @throws IllegalArgumentException
+	 *             if either the <code>stream</code> or the <code>attributes</code> are null.
+	 */
+	public static java.nio.ByteBuffer compressImageStream(java.io.InputStream inputStream, DXTCompressionAttributes attributes) {
+		if (inputStream == null) {
+			String message = Logging.getMessage("nullValue.InputStreamIsNull");
+			Logging.error(message);
+			throw new IllegalArgumentException(message);
+		}
+
+		if (attributes == null) {
+			String message = Logging.getMessage("nullValue.AttributesIsNull");
+			Logging.error(message);
+			throw new IllegalArgumentException(message);
+		}
+		
+		Options opts = new BitmapFactory.Options();
+		//opts.inPreferQualityOverSpeed = false;
+		opts.inPreferredConfig = Config.ARGB_8888;
+        Bitmap image = BitmapFactory.decodeStream(inputStream, null, opts);
+        
+		if (image == null) {
+			return null;
+		}
+
+		ETC1DDSCompressor compressor = new ETC1DDSCompressor();
+		return compressor.compressImage(image, attributes);
+	}
+	
+	/**
+	 * Converts the specified <code>image</code> to DDS according to the <code>attributes</code>. If the caller
+	 * specified a DXT format in the attributes, then we return a compressor matching that format. Otherwise, we choose
+	 * one automatically from the image type. If no choice can be made from the image type, we default to using a DXT3
+	 * compressor.
+	 * 
+	 * @param image
+	 *            image to convert to the DDS file format.
+	 * @param attributes
+	 *            attributes that control the compression.
+	 * @return buffer little endian ordered ByteBuffer containing the dds file bytes.
+	 * @throws IllegalArgumentException
+	 *             if either <code>image</code> or <code>attributes</code> are null, or if <code>image</code> has non power of two dimensions.
+	 */
+	public java.nio.ByteBuffer compressImage(Bitmap image, DXTCompressionAttributes attributes) {
+		if (image == null) {
+			String message = Logging.getMessage("nullValue.ImageIsNull");
+			Logging.error(message);
+			throw new IllegalArgumentException(message);
+		}
+		if (!WWMath.isPowerOfTwo(image.getWidth()) || !WWMath.isPowerOfTwo(image.getHeight())) {
+			String message = Logging.getMessage("generic.InvalidImageSize", image.getWidth(), image.getHeight());
+			Logging.error(message);
+			throw new IllegalArgumentException(message);
+		}
+		if (attributes == null) {
+			String message = Logging.getMessage("nullValue.AttributesIsNull");
+			Logging.error(message);
+			throw new IllegalArgumentException(message);
+		}
+
+		DXTCompressor compressor = this.getDXTCompressor(image, attributes);
+		return this.doCompressImage(compressor, image, attributes);
+	}
+	
+	protected DDSHeader createDDSHeader(DXTCompressor compressor, Bitmap image, DXTCompressionAttributes attributes) {
+		DDSPixelFormat pixelFormat = new DDSPixelFormat();
+		pixelFormat.setFlags(pixelFormat.getFlags() | DDSConstants.DDPF_FOURCC);
+		pixelFormat.setFourCC(compressor.getDXTFormat());
+
+		DDSHeader header = new DDSHeader();
+		header.setFlags(header.getFlags() | DDSConstants.DDSD_WIDTH | DDSConstants.DDSD_HEIGHT | DDSConstants.DDSD_LINEARSIZE | DDSConstants.DDSD_PIXELFORMAT
+				| DDSConstants.DDSD_CAPS);
+		header.setWidth(image.getWidth());
+		header.setHeight(image.getHeight());
+		header.setLinearSize(compressor.getCompressedSize(image, attributes));
+		header.setPixelFormat(pixelFormat);
+		header.setCaps(header.getCaps() | DDSConstants.DDSCAPS_TEXTURE);
+
+		return header;
+	}
+	
+	/**
+	 * Documentation on the DDS header format is available at <a
+	 * href="http://msdn.microsoft.com/en-us/library/bb943982(VS.85).aspx">http://msdn.microsoft.com/en-us/library/bb943982(VS.85).aspx</a>.
+	 * 
+	 * @param header
+	 *            header structure to write.
+	 * @param buffer
+	 *            buffer that receives the header structure bytes.
+	 */
+	protected void writeDDSHeader(DDSHeader header, java.nio.ByteBuffer buffer) {
+		int pos = buffer.position();
+
+		buffer.putInt(header.getSize()); // dwSize
+		buffer.putInt(header.getFlags()); // dwFlags
+		buffer.putInt(header.getHeight()); // dwHeight
+		buffer.putInt(header.getWidth()); // dwWidth
+		buffer.putInt(header.getLinearSize()); // dwLinearSize
+		buffer.putInt(header.getDepth()); // dwDepth
+		buffer.putInt(header.getMipMapCount()); // dwMipMapCount
+		buffer.position(buffer.position() + 44); // dwReserved1[11] (unused)
+		this.writeDDSPixelFormat(header.getPixelFormat(), buffer); // ddpf
+		buffer.putInt(header.getCaps()); // dwCaps
+		buffer.putInt(header.getCaps2()); // dwCaps2
+		buffer.putInt(header.getCaps3()); // dwCaps3
+		buffer.putInt(header.getCaps4()); // dwCaps4
+		buffer.position(buffer.position() + 4); // dwReserved2 (unused)
+
+		buffer.position(pos + header.getSize());
+	}
+	
+	/**
+	 * Documentation on the DDS pixel format is available at <a
+	 * href="http://msdn.microsoft.com/en-us/library/bb943984(VS.85).aspx">http://msdn.microsoft.com/en-us/library/bb943984(VS.85).aspx</a>.
+	 * 
+	 * @param pixelFormat
+	 *            pixel format structure to write.
+	 * @param buffer
+	 *            buffer that receives the pixel format structure bytes.
+	 */
+	protected void writeDDSPixelFormat(DDSPixelFormat pixelFormat, java.nio.ByteBuffer buffer) {
+		int pos = buffer.position();
+
+		buffer.putInt(pixelFormat.getSize()); // dwSize
+		buffer.putInt(pixelFormat.getFlags()); // dwFlags
+		buffer.putInt(pixelFormat.getFourCC()); // dwFourCC
+		buffer.putInt(pixelFormat.getRGBBitCount()); // dwRGBBitCount
+		buffer.putInt(pixelFormat.getRBitMask()); // dwRBitMask
+		buffer.putInt(pixelFormat.getGBitMask()); // dwGBitMask
+		buffer.putInt(pixelFormat.getBBitMask()); // dwBBitMask
+		buffer.putInt(pixelFormat.getABitMask()); // dwABitMask
+
+		buffer.position(pos + pixelFormat.getSize());
+	}
+
+	protected java.nio.ByteBuffer createBuffer(int size) {
+		return java.nio.ByteBuffer.allocateDirect(size);
+	}
+	
 	protected ByteBuffer doCompressImage(DXTCompressor compressor, Bitmap image, DXTCompressionAttributes attributes) {
 		// Create the DDS header structure that describes the specified image, compressor, and compression attributes.
 		DDSHeader header = this.createDDSHeader(compressor, image, attributes);
